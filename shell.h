01bdc6789c4b8fefa8eacdce3d137556a562adf6
kier-ious <kier.diemer@gmail.com>
2023-07-26 14:31:59 -0700

    Wrote Authors file

--- shell.h
+++ shell.h
@@ -1,11 +1,33 @@
 #ifndef SHELL_H
 #define SHELL_H
+#define DEHORS 0
+#define DEDANS 1
+#define BUF 1000
 
-
-
-
-
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdarg.h>
+#include <signal.h>
+#include <inttypes.h>
+#include <errno.h>
+extern char **environ;
+/* printf */
+#define BUFSIZE 1024
+/**
+ * struct type - structure type
+ * @c: character to check
+ * @ptr_f: pointer of function
+ */
+typedef struct type
+{
+	char c;
+	int (*ptr_f)(va_list);
+} type;
 
 
 #endif
