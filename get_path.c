abd9374fef5729a6fee86c345d29c58c3f265a6b
kier-ious <kier.diemer@gmail.com>
2023-08-03 11:56:26 -0700

    Pushing get_path function

---
+++ get_path.c
@@ -0,0 +1,42 @@
+#include "test.h"
+
+
+int get_path(char *line)
+{
+    char *path_point = NULL;
+    char *token;
+    char **env = environ;
+
+    // Find the "PATH" variable in the environment
+    while (*env != NULL)
+    {
+        if (strncmp(*env, "PATH=", 5) == 0)
+        {
+            path_point = *env + 5; // Skip "PATH=" prefix to get the actual path
+            break;
+        }
+        env++;
+    }
+
+    if (path_point == NULL)
+    {
+        // "PATH" variable not found in the environment
+        printf("PATH variable not found.\n");
+        return (1);
+    }
+
+    // Use strtok to tokenize the PATH string
+    token = strtok(path_point, DELIMITER);
+
+    // Loop to get the rest of the tokens
+    while (token != NULL)
+    {
+        // Process the token (e.g., print it)
+        printf("%s\n", token);
+
+        // Use NULL as the first argument to get the next token
+        token = strtok(NULL, DELIMITER);
+    }
+
+    return (0);
+}
