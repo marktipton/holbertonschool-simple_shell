2177e679f98065f44f3193f639a2b815d1849cc5
marktipton <6675@holbertonstudent.com>
2023-08-04 09:12:06 -0700

    took out print statements used for testing

--- check_status.c
+++ check_status.c
@@ -6,11 +6,11 @@ int check_status(const char *filename)
 	
         if (stat(filename, &fileInfo) != 0)
         {
-                printf("file does not exist");
-                perror("Error in stat");
-                return -1;
+                /*printf("file does not exist");*/
+                /*perror("Error in stat");*/
+                return (-1);
         }
-        else
+        /*else
         {
                 printf("File: %s\n", filename);
                 printf("File permissions: %o\n", fileInfo.st_mode);
@@ -18,7 +18,7 @@ int check_status(const char *filename)
                 printf("Last modification time: %ld\n", fileInfo.st_mtime);
                 printf("Last status change time: %ld\n", fileInfo.st_ctime);
 		printf("File: %s\n", filename);
-        }
+        }*/
         return (0);
 }
 int check_access(const char *file_path)
@@ -26,13 +26,13 @@ int check_access(const char *file_path)
     	/*Check if the file exists and is executable*/
     	if (access(file_path, X_OK) == 0)
 	{
-		printf("You have access to execute the file: %s\n", file_path);
-		return 1; /*Access granted*/
+		/*printf("You have access to execute the file: %s\n", file_path);*/
+		return (1); /*Access granted*/
 	}
 	else
 	{
 		perror("access");
-		printf("You do not have access to execute the file: %s\n", file_path);
-		return 0;
+		/*printf("You do not have access to execute the file: %s\n", file_path);*/
+		return (0);
 	}
 }
