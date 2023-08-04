b03fd1f43d1dacdb767ec74371c5d2b5408d142c
kier-ious <kier.diemer@gmail.com>
2023-08-04 15:26:34 -0700

    We cleaned up some files and added ability to execute full path copmmands after checking status and access

--- execute_command.c
+++ execute_command.c
@@ -9,13 +9,8 @@
 int execute_command(char *line) 
 {
 	pid_t child_pid;
-	/*char input[100] = {0};*/
 	int status;
-	char *binstr = "/bin";
-	char *exec_file_name;
 	char **tokstr;
-
-	line[strcspn(line, "\n")] = '\0'; /*Fix the line variable name*/
 
 	child_pid = fork();
 
@@ -28,26 +23,16 @@ int execute_command(char *line)
 	{	
 		tokstr = tokenizer(line, WHITESPACE);
 
-		if (strcmp(tokstr[0], "ls") == 0) /*Check if command is builtin*/
-		{
-			if (execvp("/bin/ls", tokstr) == -1)
+			status = execve(line, tokstr, environ);
+			if (status == -1)
 			{
-				perror("Error executing ls");
+				perror("execve");
 				exit(EXIT_FAILURE);
 			}
-		}
-		else
-		{
-			exec_file_name = strcat(binstr, tokstr[0]);
-			if (execve(exec_file_name, tokstr, environ) == -1)
-			{
-				exit(EXIT_FAILURE);
-			}
-		}
 	}
 	else /*This is the parent process*/
 	{
-		wait(&status); /*Wait for the child process to finish*/
+		wait(&status);
 	}
 	return (0);
 }
